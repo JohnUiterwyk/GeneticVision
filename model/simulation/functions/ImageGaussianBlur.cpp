42bf9a5a12904c69388b46f76ae5744389eca4ee
John Uiterwyk <jnuiterwyk@gmail.com>
2015-05-19 15:03:12 +1000

    add gaussian blur

---
+++ model/simulation/functions/ImageGaussianBlur.cpp
@@ -0,0 +1,5 @@
+//
+// Created by John Uiterwyk on 5/19/15.
+//
+
+#include "ImageGaussianBlur.h"
---
+++ model/simulation/functions/ImageGaussianBlur.h
@@ -0,0 +1,14 @@
+//
+// Created by John Uiterwyk on 5/19/15.
+//
+
+#ifndef GENETICVISION_IMAGEGAUSSIANBLUR_H
+#define GENETICVISION_IMAGEGAUSSIANBLUR_H
+
+
+class ImageGaussianBlur {
+
+};
+
+
+#endif //GENETICVISION_IMAGEGAUSSIANBLUR_H
