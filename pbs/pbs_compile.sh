5f492a2a7e99324f219ce5cb5f8703999cbe766d
John Uiterwyk <jnuiterwyk@gmail.com>
2015-06-03 21:22:50 +1000

    revise pubs compile job script

--- pbs/pbs_compile.sh
+++ pbs/pbs_compile.sh
@@ -21,18 +21,24 @@
 #       Note: Please ensure that the PBS -M option above is set.
 #
 #PBS -m abe
-
+ROOT_DIR=$PBS_O_WORKDIR/..
+BUILD_DIR=$PBS_O_WORKDIR/../build
+OUTPUT_DIR=$PBS_O_WORKDIR/../output
+STDOUT_LOG=$PBS_O_WORKDIR/../output/pbs_gv_stdout.log
 
 # Changes directory to your execution directory (Leave as is)
 cd $PBS_O_WORKDIR
+cd $ROOT_DIR
+
+mkdir $OUTPUT_DIR
 
 # Load the environment variables for R
 # module load R
-module load cmake >> $PBS_O_WORKDIR/output/pbs_gv_stdout.log
+module load cmake >> OUTPUT_DIR/pbs_gv_stdout.log
 
 # The command to actually run the job
-mkdir build >> $PBS_O_WORKDIR/output/pbs_gv_stdout.log
-cd build >> $PBS_O_WORKDIR/output/pbs_gv_stdout.log
-cmake .. >> $PBS_O_WORKDIR/output/pbs_gv_stdout.log
-make >> $PBS_O_WORKDIR/output/pbs_gv_stdout.log
-
+mkdir $BUILD_DIR
+cd $BUILD_DIR >> $STDOUT_LOG
+cmake ROOT_DIR >> $STDOUT_LOG
+make >> $STDOUT_LOG
+cd $PBS_O_WORKDIR
