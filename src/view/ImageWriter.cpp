3a9c5d5405ba3ed80ab4c14baf5e0dda3e63743b
John Uiterwyk <jnuiterwyk@gmail.com>
2015-06-08 16:01:16 +1000

    add image writer class

---
+++ src/view/ImageWriter.cpp
@@ -0,0 +1,9 @@
+//
+// Created by John Uiterwyk on 6/8/15.
+//
+
+#include "ImageWriter.h"
+namespace GeneticVision
+{
+
+}
---
+++ src/view/ImageWriter.h
@@ -0,0 +1,25 @@
+//
+// Created by John Uiterwyk on 6/8/15.
+//
+
+#ifndef GENETICVISION_IMAGEWRITER_H
+#define GENETICVISION_IMAGEWRITER_H
+
+#include "../AppConfig.h"
+#include "../model/simulation/RunResult.h"
+
+namespace GeneticVision
+{
+    class ImageWriter {
+    public:
+        ImageWriter(AppConfig *appConfig);
+        void update(RunResult *runResult);
+    private:
+        int writeFrequency;
+        string outputDirectory;
+    };
+
+}
+
+
+#endif //GENETICVISION_IMAGEWRITER_H
