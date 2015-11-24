17fac288ef5145d4cbc3871e403eaa8f283ff23f
John Uiterwyk <jnuiterwyk@gmail.com>
2015-11-24 17:26:51 -0500

    add more comments to compile script

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
+# create a unique id for the build
 UUID=$(uuidgen)
 BUILD_DIR=build-$UUID
 
+# create the build and bin directories
 mkdir -p $BUILD_DIR
 mkdir -p bin
 
@@ -9,14 +11,20 @@ mkdir -p bin
 # Changes directory to your execution directory (Leave as is)
 
 
+# Change to build directory
+cd $BUILD_DIR
 
 # Perform out of source build using build folder
-cd $BUILD_DIR
 cmake ..
 make
+
+# Change back to source directory
 cd ..
+
+# Move the resulting binary to bin directory
 echo "Moving GeneticVision to $(pwd)/bin"
 mv $BUILD_DIR/GeneticVision bin
+
 echo "Deleting temp build directory $BUILD_DIR"
 rm -rf $BUILD_DIR
 echo "Finished at $(date)"
